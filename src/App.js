22d6c0d1ff6f3deabd5292e2cde9c2ea34f12d4e
julezKiano <jgkiano@gmail.com>
2018-06-25 10:24:44 +0300

    initial commit starter dependancies added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Lets do this!
+            </div>
+        );
+    }
+}
+
+export default App;
