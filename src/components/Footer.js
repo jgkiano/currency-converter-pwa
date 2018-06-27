bc5a07c1badd383db806ceb8996b5888cd4c8dc0
julezKiano <jgkiano@gmail.com>
2018-06-27 19:17:27 +0300

    added footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,32 @@ import styled from 'styled-components';
 
 const FooterContainer = styled.div`
     width: 100%;
-    background-color: #424242;
+    background-color: #E0E0E0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #616161;
+    height: 50px;
 `;
+
+const Link = styled.a`
+    display: inline-block;
+    margin-left: 5px;
+    text-decoration: none;
+    color: #616161;
+    &:hover {
+        color: #212121;
+    }
+`;
+
+const FOOTERTEXT = '</> by';
 
 class Footer extends Component {
     render() {
         return (
             <FooterContainer>
-                <span>Made by @kiano</span>
+                <span>{FOOTERTEXT}</span>
+                <Link target="_blank" href="https://github.com/julezKiano">@kiano</Link>
             </FooterContainer>
         );
     }
