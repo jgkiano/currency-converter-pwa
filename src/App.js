4ef2901efeaa361428d4449afb1745ae7e44e64d
julezKiano <jgkiano@gmail.com>
2018-06-28 21:35:44 +0300

    width for desktop comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+//> 960 is desktop
 class CurrencyConverter extends Component {
     render() {
         return (
